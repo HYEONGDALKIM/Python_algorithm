0e5599ecf1fac65ac97d8711cd80d7995908c04e
HYEONGDALKIM <guddak@kakao.com>
2021-11-24 00:13:41 +0900

    두 수 비교하기

--- if문/1330.py
+++ if문/1330.py
@@ -1,5 +1,11 @@
 # 문제
 # 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
 
-A = int(input())
-B = int(input())
+A, B = map(int,input().split())
+
+if A>B:
+	print(">")
+elif A<B:
+	print("<")
+elif A==B:
+	print("==")
