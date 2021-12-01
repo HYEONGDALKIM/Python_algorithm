8d8d84d25118a00443da179225f626fa082decd4
HYEONGDALKIM <guddak@kakao.com>
2021-12-01 15:36:00 +0900

    최댓값

---
+++ Q_about_OneArray/2562.py
@@ -0,0 +1,19 @@
+# numbers = []
+# for i in range(9):
+#     i = int(input())
+#     numbers.append(i)
+    
+# print(max(numbers))
+# print(numbers.index(max(numbers))+1)
+
+
+arr = []
+for i in range(9):
+    i = int(input())
+    arr.append(i)
+    
+print(max(arr))
+print(arr.index(max(arr))+1)
+
+
+
